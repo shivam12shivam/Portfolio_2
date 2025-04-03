3fdc17ea984c68431f869cc93a4689d0474befdc
shivam <shivam198237@gmail.com>
2025-04-03 14:23:57 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -18,14 +19,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      
+
     <html lang="en">
       <body
         className={clsx(
           inter.variable,
           "bg-background text-foreground font-inter"
         )}
-      >{children}</body>
+      >{children}
+        <Analytics /></body>
     </html>
 
   );
